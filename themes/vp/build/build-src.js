51542b38f8a9a8fbb8c4bc1e8daa6bf6a1393649
Dustin Quam <dustinquam@me.com>
2015-04-07 16:05:08 -0500

    updated ss + modules

--- themes/vp/build/build-src.js
+++ themes/vp/build/build-src.js
@@ -11325,6 +11325,12 @@ $(function() {
 			minItems: 1,
 			maxItems: 1,
 			itemWidth: 500,
+			start: function(){
+				bLazy.revalidate();
+			},
+			before: function(){
+				bLazy.revalidate();
+			},
 		});
 	}
 
