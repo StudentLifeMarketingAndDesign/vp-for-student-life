c65987522d10759f3c349df2b3ecce92b67d99d0
Dustin Quam <dustinquam@me.com>
2016-10-28 13:24:34 -0500

    theme consolidation, content blocks

--- mysite/_config.php
+++ mysite/_config.php
@@ -11,10 +11,6 @@ require_once("conf/ConfigureFromEnv.php");
 
 MySQLDatabase::set_connection_charset('utf8');
 
-// Set the current theme. More themes can be downloaded from
-// http://www.silverstripe.org/themes/
-SSViewer::set_theme('simple');
-
 // Set the site locale
 i18n::set_locale('en_US');
 FulltextSearchable::enable();
