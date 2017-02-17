2f2e8f46272aa19fef6500e5d5522f1fe317574c
Dustin Quam <dustinquam@me.com>
2017-02-17 14:44:35 -0600

    updated dp and added remove remember relations task

---
+++ mysite/code/tasks/RemoveMemberRelationsTask.php
@@ -0,0 +1,25 @@
+<?php
+
+	class RemoveMemberRelationsTask extends BuildTask {
+
+
+		protected $title = 'Remove all member/blog post relationships';
+		protected $description = 'Disassociates members from posts and defaults to the "Additional Authors" field';
+
+		protected $enabled = true;
+
+		function run($request){
+			$posts = BlogPost::get();
+			echo '<p>Gathering all posts...</p>';
+			echo '<ul>';
+			foreach($posts as $post){
+				echo '<li>Removing authors from <strong>'.$post->Title.'</strong></li>';
+				$post->Authors()->removeAll();
+			}
+			echo '</ul>';
+			echo 'Done.';
+
+		}
+
+
+	}
