04bf8c4a689b50989682b5b0812fa93cde85e44c
Dustin Quam <dustinquam@me.com>
2017-03-27 16:36:33 -0500

    FB feed import

---
+++ mysite/code/tasks/FBFeedImportTask.php
@@ -0,0 +1,54 @@
+<?php
+
+use Olliepop\FBPageFeed\FBPageFeedService;
+use Olliepop\FBPageFeed\FacebookPost;
+
+
+//      Run every 10 minutes. Store in crontab:
+//      */10 * * * * /var/www/vhosts/nzse.ac.nz/httpdocs/framework/sake FBPageFeed "flush=1"
+
+/*
+ * Class FBPageFeedTask
+ */
+
+class FBFeedImportTask extends BuildTask
+{
+
+    /**
+     * @var
+     */
+    private $fbService;
+
+    /**
+     * Initiate the service and copy new posts to our database
+     */
+    public function run($request)
+    {
+        $this->fbService = new FBPageFeedService();
+
+        $storedPosts = $this->fbService->getStoredPosts();
+        $posts = $this->fbService->getPostsFromFacebook();
+        $inserted = 0;
+        foreach ($posts as $i => $post) {
+            if (!isset($post['FBID'])) break;
+
+            $existingPost = FacebookPost::get()->filter('URL', $post['URL'])->first();
+
+            if ($existingPost) {
+                break;
+            } else {
+                if (isset($post['source'])) {
+                    $imageSource = $post['source'];
+                } else {
+                    $imageSource = null;
+                }
+
+                $this->fbService->storePost($post['FBID'], $post['Content'], $post['URL'], $post['TimePosted'], $imageSource);
+                $inserted++;
+            }
+        }
+
+        echo 'Stored ' . $inserted . ' new posts.';
+    }
+
+}
